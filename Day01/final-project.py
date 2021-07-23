e82b224037024252d74e7c4eae84c92e231cf779
fadelakbarputra <fadelakbarputra19@gmail.com>
2021-07-23 11:13:01 +0700

    menyelesaikan projek day 1

---
+++ Day01/final-project.py
@@ -0,0 +1,10 @@
+# print("Welcome to the Brand name Generator")
+# print("Dimana kamu tinggal?")
+# tempat = input("")
+# print("Apa nama hewa peliharaanmu? ")
+# pet = input("")
+
+city = input("Di mana kamu tinggal?\n")
+pet = input("Apa nama hewan peliharaanmu?\n")
+
+print("Nama Band yang dibentuk adalah: " + city + " " + pet)
