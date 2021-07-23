ae167b21bc9dafb7d1db47d18826bae34fde4925
fadelakbarputra <fadelakbarputra19@gmail.com>
2021-07-23 17:31:55 +0700

    membuat file 01-convertvar.py

---
+++ Day02/01-convertvar.py
@@ -0,0 +1,9 @@
+# print("Umur kamu " + 123 + " tahun") #error
+
+num_length = len(input("What is ur name?"))
+# konversi dari int ke string
+num_length = str(num_length)
+print("Nama kamu memiliki " + num_length + " huruf")
+
+print(12 + float("123.5")) # akan menjumlahkan biasa
+print(str(1231) + str(83)) # akan membuat string
