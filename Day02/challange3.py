7f53524e126e8d9571e4d490b094e4a271d9b3ec
fadelakbarputra <fadelakbarputra19@gmail.com>
2021-07-23 18:55:57 +0700

    menyelesaikan challange3.py

---
+++ Day02/challange3.py
@@ -0,0 +1,9 @@
+# asumsi umur maks adalah 90 dan 365 hari, 52 minggu, 12 bulan
+# semua yang dimasukkan ke input bertipe string, jd harus dikonversi dulu
+age = int(input("Berapa umurmu sekarang? "))
+
+current_month = (90 - age) * 12
+current_week = (90 - age) * 52
+current_day = (90 - age) * 365
+
+print(f"You have {current_day} days left, {current_week} weeks left, and {current_month} month left")
