097ab00147f9db3d080ea8e1ed4b93058868b603
fadelakbarputra <fadelakbarputra19@gmail.com>
2021-07-23 18:44:55 +0700

    membuat file ftring.py

---
+++ Day02/02-fstring.py
@@ -0,0 +1,10 @@
+# pembagian oada python otomatis adalah float
+print(8 / 3)
+print(round(8 / 3, 2))
+print(8 // 3) # type int
+
+score = 90
+height = 1.87
+isWin = True
+#f-string
+print(f"My score is {score}, my height is {height}, and am i winning? {isWin}")
