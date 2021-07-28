6c21fa34946b932693df46ce6f82637005b21d78
fadelakbarputra <fadelakbarputra19@gmail.com>
2021-07-28 14:29:39 +0700

    menyelesaikan nestedlist

---
+++ Day04/04-nestedlist.py
@@ -0,0 +1,6 @@
+vegetables = ["Kangkung", "Bayam", "Kacang", "Seledri", "Kemangi"]
+fruits = ["Apple", "Orange", "Mango", "Grape", "Guava"]
+
+my_fav = [vegetables, fruits]
+print(my_fav)
+print(my_fav[1][0])
