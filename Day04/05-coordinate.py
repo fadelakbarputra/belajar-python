972d19e3a6168e1cf469f0f3928af29a8db7e586
fadelakbarputra <fadelakbarputra19@gmail.com>
2021-08-02 13:44:37 +0700

    mengubah var input

--- Day04/05-coordinate.py
+++ Day04/05-coordinate.py
@@ -3,7 +3,7 @@ row2 = ["⬜️","⬜️","⬜️"]
 row3 = ["⬜️","⬜️","⬜️"]
 map = [row1, row2, row3]
 print(f"{row1}\n{row2}\n{row3}")
-coordinate = str(input("Enter coloumn then row: \n"))
+coordinate = input("Enter coloumn then row: \n")
 
 x_axis = int(coordinate[0]) - 1
 y_axis = int(coordinate[1]) - 1
